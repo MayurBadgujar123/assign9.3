1d0234291f96c2896b0d604f92073c7ca73a7aa6
MayurBadgujar123 <132994310+MayurBadgujar123@users.noreply.github.com>
2023-08-24 22:12:18 +0530

    Add files via upload

---
+++ InheritanceExamples2/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace InheritanceExamples2
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            DerivedClass o = new DerivedClass();
+            o.Display1();
+            o.Display1("vik");
+
+            o.Display2();
+            o.Display3();
+        }
+
+        static void Main()
+        {
+            BaseClass o;
+            o = new BaseClass();
+            //o.Display2(); //non virtual - will depend on how the reference is declared
+            o.Display3();  //virtual - will depend on how memory is allocated
+
+            Console.WriteLine();
+
+            o = new DerivedClass();
+            //o.Display2(); //non virtual - will depend on how the reference is declared
+            o.Display3();  //virtual - will depend on how memory is allocated
+
+
+            Console.WriteLine();
+            o = new SubDerivedClass();
+            //o.Display2(); //non virtual - will depend on how the reference is declared
+            o.Display3();  //virtual - will depend on how memory is allocated
+
+            Console.WriteLine();
+            o = new SubSubDerivedClass();
+            //o.Display2(); //non virtual - will depend on how the reference is declared
+            o.Display3();  //virtual - will depend on how memory is allocated
+
+
+        }
+
+    }
+    public class BaseClass
+    {
+        public void Display1()
+        {
+            Console.WriteLine("base display1");
+        }
+        public void Display2()
+        {
+            Console.WriteLine("base display2");
+        }
+        public virtual void Display3()
+        {
+            Console.WriteLine("base display3");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        //overload the base class method
+        public void Display1(string s)
+        {
+            Console.WriteLine("derived display1" + s);
+        }
+
+        //hide the base class method
+        public new void Display2()
+        {
+            Console.WriteLine("derived display2");
+        }
+        //override the base class method
+        public override void Display3()
+        {
+            Console.WriteLine("derived display3");
+        }
+
+    }
+    public class SubDerivedClass : DerivedClass
+    {
+        public sealed override void Display3()
+        {
+            Console.WriteLine("subderived display3");
+        }
+
+    }
+
+    public class SubSubDerivedClass : SubDerivedClass
+    {
+        public new void Display3()
+        {
+            Console.WriteLine("subsubderived display3");
+        }
+
+    }
+}
---
+++ InterfacesWithDefaultMethods/InterfacesWithDefaultMethods/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace InterfacesWithDefaultMethods
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            Class1 o = new Class1();
+            o.Insert();
+
+            IDbFunctions oIDb;
+            oIDb = o;
+
+            oIDb.Select();  //def imp select called
+        }
+
+        static void Main()
+        {
+            Class1 o = new Class1();
+            //o.Select();  //class1 select called -- can only be called this way if it is public
+
+            IDbFunctions oIDb;
+            oIDb = o;
+
+            oIDb.Select(); //class1 select called
+        }
+    }
+    public interface IDbFunctions
+    {
+        void Insert();
+        void Update();
+        void Delete();
+        void Select()  //default implementation
+        {
+            Console.WriteLine("default implementation of select");
+        }
+
+    }
+    public class Class1 : IDbFunctions
+    {
+        public void Display()
+        {
+            Console.WriteLine("class1 display");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("class1 Idbfunctions delete");
+        }
+        public void Insert()
+        {
+            Console.WriteLine("class1 Idbfunctions insert");
+        }
+        public void Update()
+        {
+            Console.WriteLine("class1 Idbfunctions update");
+        }
+        //public void Select()  //implement Select()
+        //{
+        //    Console.WriteLine("class1 Idbfunctions select");
+        //}
+        void IDbFunctions.Select()  //implement Select() EXPLICITLY
+        {
+            Console.WriteLine("class1 Idbfunctions select - EXPLICITLY");
+        }
+    }
+
+}
